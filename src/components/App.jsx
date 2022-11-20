1d3689ed38f6f5a8f74ce3e04071f49ef904212a
AM1007 <andrew.motko@gmail.com>
2022-11-20 07:52:12 +0200

    transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './Transaction/transactions.json';
 
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './Transaction/TransactionHistory';
 
 export const App = () => {
   return (
@@ -27,7 +30,8 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
-      
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
